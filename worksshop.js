23acda9d409b5c13e45fd5eaad5b5d838292ae08
Sam <samspeciale1997@gmail.com>
2023-05-09 13:21:48 -0500

    workshop for block18 shopping cart

---
+++ worksshop.js
@@ -0,0 +1,53 @@
+
+
+// Unit Tests 
+
+// ### Prompt 1
+
+//   A function called "multiplication" that returns the product of the two input numbers.
+
+//   Prompt 1 Tests -
+
+//     1. returns the right product of two numbers
+// 2. returns the right product of two positive numbers
+// 3. returns the right producto of two negative numbers
+// 4. returns the right product when a number is multiplied by 0
+
+// ### Prompt 2
+
+//  A function called "concatOdds" takes two arrays of integers as arguments.It should return a single array that only contains the odd numbers, in ascending order, from both of the arrays.
+
+//   Prompt 2 Tests -
+
+//     1. it returns an array of odd numbers in ascending order from the two arrays
+// 2. returns an empty array if both the arrays have no odd numbers
+// 3. it returns an array of all the numbers from both arrays when they contain only odd numbers
+
+// 2. concatOdds([3, 2, 1], [9, 1, 1, 1, 4, 15, -1]) => [-1, 1, 3, 9, 15]
+
+// 2.concatOdds([...
+
+//   Prompt 2 - ......
+
+// 1. ...
+
+// 2. ...
+
+// Functional Tests
+
+// // ...
+
+// Shopping cart workshop
+
+// A shopping cart checkout feature that allows a user to check out as a guest(without an account), or as a logged -in user.They should be allowed to do either, but should be asked if they want to create an account or log in if they check out as a guest.
+// Think about the following; you may need to make assumptions or decisions about the prompt and how the feature should behave:
+// What should happen if the cart is empty ?
+//   What needs to be shown to the user at each step of check out ?
+//     What behaviors of this feature does the prompt miss or gloss over ?
+//       Hint : Observe the shopping cart and checkout features of some popular websites to get some ideas!
+// Note: Please create a file in your vscode and write the tests as comments in there.You can push that file to github then and share it the usual way with us.Also note, this workshop does not involve coding it's a theoretical testing exercise. Your file may look like : 
+
+// 1. test that a user is able to check out as a guest without an account
+// 2. test that a user is able to check out as a loffed in user with an accound
+// 3. test that a user when lands on the homepage, sees a popup that asks if they want to sign in or continue as a guest
+// 4. test that a user is unable to checkout with nothihng in the cart
